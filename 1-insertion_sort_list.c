990a237cc8857a97d2b9ca914496fa75850fa7be
Salma_Salah <salma.s.kabel@gmail.com>
2023-09-22 10:08:44 -0700

    answer

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,33 +1,30 @@
 #include "sort.h"
 
-/**
- * insertion_sort_list - sorts a doubly linked list
- * of integers in ascending order
- * @list: list to sort
- */
-
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *ptr, *ptr2;
+    listint_t *current, *next_node;
 
-	ptr = *list;
-	while (ptr != NULL)
-	{
-		ptr2 = ptr->next;
-		while (ptr->prev != NULL && ptr->n < ptr->prev->n)
-		{
-			if (ptr->next != NULL)
-				ptr->next->prev = ptr->prev;
-			if (ptr->prev->prev == NULL)
-				*list = ptr;
-			else
-				ptr->prev->prev->next = ptr;
-			ptr->prev->next = ptr->next;
-			ptr->next = ptr->prev;
-			ptr->prev = ptr->prev->prev;
-			ptr->next->prev = ptr;
-			print_list(*list);
-		}
-		ptr = ptr2;
-	}
+    if (list == NULL || *list == NULL)
+        return;
+
+    current = (*list)->next;
+    while (current != NULL)
+    {
+        next_node = current->next;
+        while (current->prev != NULL && current->prev->n > current->n)
+        {
+            current->prev->next = current->next;
+            if (current->next != NULL)
+                current->next->prev = current->prev;
+            current->next = current->prev;
+            current->prev = current->prev->prev;
+            current->next->prev = current;
+            if (current->prev != NULL)
+                current->prev->next = current;
+            else
+                *list = current;
+            print_list(*list);
+        }
+        current = next_node;
+    }
 }
