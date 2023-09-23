39e81c74bf017bb6bb5671dfb1505dc06b6dd7ea
Salma_Salah <salma.s.kabel@gmail.com>
2023-09-23 07:09:42 -0700

    answer

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,52 +1,36 @@
 #include "sort.h"
 
-/**
- * shell_sort - sorts an array of integers in ascending order
- * using the Shell sort algorithm, using the Knuth sequence
- * @array: array to sort
- * @size: size of array
- */
-
 void shell_sort(int *array, size_t size)
 {
-	int h = 1, swap = 1, tmp, k, j;
+    int h = 1;
+    size_t i, j;
+    int tmp;
 
-	if (array == NULL || size <= 0)
-		return;
-	while ((h * 3 + 1) < (int)size)
-		h = h * 3 + 1;
-	while (h > 1)
-	{
-		while (swap == 1)
-		{
-			swap = 0;
-			for (j = 0; j < (int)size; j++)
-			{
-				if (j + h < (int)size && array[j] > array[j + h])
-				{
-					swap = 1;
-					tmp = array[j + h];
-					array[j + h] = array[j];
-					array[j] = tmp;
-				}
-			}
-		}
-		print_array(array, size);
-		h = (h)/ 3;
-	}
-	j = 1;
-	while (j < (int)size)
-	{
-		k = j;
-		while (k > 0 && array[k] < array[k - 1])
-		{
-			tmp = array[k - 1];
-			array[k - 1] = array[k];
-			array[k] = tmp;
-			k--;
-		}
-		j++;
-		if (j == (int)size)
-			print_array(array, size);
-	}
+    if (array == NULL || size < 2)
+        return;
+
+    /* Generate the Knuth sequence */
+    while (h < (int)size/3)
+        h = 3 * h + 1;
+
+    /* Start with the largest gap and work down to a gap of 1 */
+    while (h >= 1)
+    {
+        /* Do a gapped insertion sort for this gap size */
+        for (i = h; i < size; i++)
+        {
+            /* Save array[i] in tmp and make a hole at position i */
+            tmp = array[i];
+
+            /* Shift earlier gap-sorted elements up until the correct location for array[i] is found */
+            for (j = i; j >= h && array[j - h] > tmp; j -= h)
+                array[j] = array[j - h];
+
+            /* Put tmp (the original array[i]) in its correct location */
+            array[j] = tmp;
+        }
+
+        print_array(array, size);
+        h /= 3;
+    }
 }
