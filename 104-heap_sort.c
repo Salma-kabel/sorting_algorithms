3f8ad96ae20414b9b9cefa69b79f964be5be18fc
Salma_Salah <salma.s.kabel@gmail.com>
2023-09-25 12:44:18 -0700

    answe

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,45 +1,70 @@
 #include "sort.h"
 
-
+/**
+ * heap_sort - sorts an array of integers in ascending
+ * order using the Heap sort algorithm
+ * @array: array to sort
+ * @size: size of array
+ */
 
 void heap_sort(int *array, size_t size)
 {
-	tree *head, *ptr, *node = NULL;
+	size_t i, j, size2;
+	int tmp;
 
 	if (array == NULL || size <= 0)
 		return;
-	head = create(node, 0, array[0], NULL);
-	for (i = 0; i < (int)size; i++)
+	i = size - 1;
+	while (i > 0)
 	{
-		ptr->left = create(node, i, array[i], ptr);
-		if (i + 1 < size)
-			ptr->right = create(node, i + 1, array[i + 1], ptr);
-		if (ptr->parent != NULL)
+		j = (i - 1) / 2;
+		if ((i - 1) % 2 != 0)
 		{
-		       if (ptr = ptr->parent->left)
-			       ptr = ptr->parent->right;
-		       else if (ptr = ptr->parent->parent->left)
-			       ptr =ptr->parent->parent->right->left;
-		       else
-			       ptr = ptr->parent->parent->left->left->left;
+			if (array[i] < array[i - 1] && array[i - 1] > array[j])
+			{
+				i--;
+				continue;
+			}
+		}
+		if (array[i] > array[j])
+		{
+			tmp = array[j];
+			array[j] = array[i];
+			array[i] = tmp;
+			i = size - 1;
+			print_array(array, size);
 		}
 		else
-			ptr = ptr->left;
+			i--;
 	}
-}
-
-tree *create(tree *node, int i, int v, tree *p)
-{
-	node = malloc(sizeof(tree));
-	if (node == NULL)
+	size2 = size;
+	while (size > 1)
 	{
-		free;
-		return;
+		tmp = array[size - 1];
+		array[size - 1] = array[0];
+		array[0] = tmp;
+		print_array(array, size2);
+		size--;
+		i = 0;
+		j = 2 * i + 1;
+		while ((j < size && array[i] < array[j]) || ( j + 1 < size && array[i] < array[j + 1]))
+		{
+			tmp = array[i];
+			if (j + 1 < size && array[i] < array[j + 1])
+			{
+				if (j < size)
+				{
+				      if (array[j + 1] > array[j])
+					      j = j + 1;
+				}
+				else
+					j = j + 1;
+			}
+			array[i] = array[j];
+			array[j] = tmp;
+			print_array(array, size2);
+			i = j;
+			j = 2 * i + 1;
+		}
 	}
-	node->location = i;
-	node->value = v;
-	node->parent = p;
-	node->left = NULL;
-	node->right = NULL;
-	return node;
 }
